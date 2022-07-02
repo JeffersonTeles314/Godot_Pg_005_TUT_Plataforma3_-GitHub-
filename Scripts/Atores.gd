ef353d8df9132397ff0b170eaf743eaf5cea3c31
Jefferson Teles <jeffersonteles007@gmail.com>
2022-07-02 15:40:57 -0500

    Commit Definitivo

--- Scripts/Atores.gd
+++ Scripts/Atores.gd
@@ -7,19 +7,21 @@ const gravidade = 15
 #=-=-=Constanstes - F=-=-=
 
 #=-=-=Constanstes do Jogador - I=-=-=
-const Aceleracao = 200
-const Velocidade_Max = 500
-const pulo = -300
-const atritochao = 0.1
+const aceleracao = 100
+const velocidade_Max = 500
+const pulo = -600
+const atritochao = 0.2
 const atritoar = 0.005
+const forcejumpbreak = 0.5
+var is_jumpbreak = false 
 #=-=-=Constanstes do Jogador - F=-=-=
 
-#=-=-=Vetor de Velocidade - I=-=-=
-var movimento = Vector2()
-#=-=-=Vetor de Velocidade - F=-=-=
+#=-=-=Vetores de Movimento - F=-=-=
+var movimento = Vector2.ZERO
+var direct = Vector2.ZERO
+#=-=-=Vetores de Movimento - F=-=-=
 
 func _physics_process(delta):
-	#=-=-=Código de Gravidade - I=-=-=
 	movimento.y += gravidade
-	#=-=-=Código de Gravidade - F=-=-=
 	pass
+
--- Scripts/Jogador.gd
+++ Scripts/Jogador.gd
@@ -1,30 +1,55 @@
 extends Atores
 
-func _physics_process(_delta):
-	#=-=-=Código-Anti-Aceleração - I=-=-=
-	movimento.x = 0
-	#=-=-=Código-Anti-Aceleração - F=-=-=
-	
-	#=-=-=Código de Pulo - I=-=-=
-	if is_on_floor() and Input.is_action_pressed("cus_ui_pular"):
-		movimento.y = pulo
-	#=-=-=Código de Pulo - F=-=-=
-	
-	#=-=-=Movimentação Direita - I=-=-=
+#=-=-=GetDirect=-=-=
+func GetDirect():
+	direct.x = 0
+	direct.y = 0
 	if Input.is_action_pressed("cus_ui_moverdireita"):
-		movimento.x = min(movimento.x + Aceleracao , Velocidade_Max)
-	#=-=-=Movimentação Direita - F=-=-=
-	
-	#=-=-=Movimentação Esquerda - I=-=-=
+		direct.x += 1
 	if Input.is_action_pressed("cus_ui_moveresquerda"):
-		movimento.x = max(movimento.x - Aceleracao , -Velocidade_Max)
-	#=-=-=Movimentação Esquerda - F=-=-=
+		direct.x -= 1
+	if is_on_floor() and Input.is_action_pressed("cus_ui_pular"):
+		direct.y += 1
+
+#=-=-=GetMov=-=-=
+func GetMov(
+mov_ins,
+direct
+):
+	#Movimento Horizontal
+	if direct.x > 0:
+		mov_ins.x = min(mov_ins.x + aceleracao * direct.x, velocidade_Max)
+	if direct.x < 0:
+		mov_ins.x = max(mov_ins.x + aceleracao * direct.x , -velocidade_Max)
+	#Movimento Horizontal
+	
+	#Movimento Vertical/Pulo
+	#Pulo Normal
+	if direct.y != 0:
+		mov_ins.y = pulo * direct.y
+	#Pulo Normal
+	
+	#Pulo Interrompido
+	if Input.is_action_just_released("cus_ui_pular") and mov_ins.y < 0.0:
+		#mov_ins.y = 0.0
+		is_jumpbreak = true
+	if is_jumpbreak == true:
+		mov_ins.y = lerp(mov_ins.y, 0, forcejumpbreak)
+		if mov_ins.y >= 0:
+			is_jumpbreak = false
+	#Pulo Interrompido
+	#Movimento Vertical/Pulo
 	
 	#=-=-=Código Atrito - I=-=-=
 	if is_on_floor():
-		movimento.x = lerp(movimento.x, 0, atritochao)
+		mov_ins.x = lerp(mov_ins.x, 0, atritochao)
 	else:
-		movimento.x = lerp(movimento.x, 0, atritoar)
+		mov_ins.x = lerp(mov_ins.x, 0, atritoar)
 	#=-=-=Código Atrito - F=-=-=
+	return mov_ins
+
+func _physics_process(delta):
+	GetDirect()
+	movimento = GetMov(movimento, direct)
 	movimento = move_and_slide(movimento, cima)
 	pass
