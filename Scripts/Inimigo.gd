8340312e4a33218b82eae8af52fb391162720014
Jefferson Teles <jeffersonteles007@gmail.com>
2022-07-03 16:45:43 -0500

    Commit

--- Scripts/Inimigo.gd
+++ Scripts/Inimigo.gd
@@ -4,8 +4,19 @@ func _ready():
 	movimento.x = -velinimg
 	set_physics_process(false)
 	
+func _on_areadepulo_body_entered(body):
+	if body.global_position.y > get_node("areadepulo").global_position.y:
+		return
+	else:
+		die()
+
+func die() -> void:
+	queue_free()
+
 func _physics_process(delta):
 	movimento.y = move_and_slide(movimento, cima).y
 	if is_on_wall():
 		movimento.x *= -1
 	pass
+
+
